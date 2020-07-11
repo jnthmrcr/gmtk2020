ba8141be52714c8fd68cd0bff494b9a2ffc92a66
Jonathan <jonathanmercer2012@yahoo.com>
2020-07-10 20:31:08 -0500

    Init
    basic camera/scene with post processing

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    [SerializeField] float inputInertiaLerp = 5f;
+    [SerializeField] float cameraTrackingLerp = 20f;
+
+    Vector2 camPosition;
+
+    Vector2 input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButton("Fire2"))
+		{
+            input = new Vector2(-Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"));
+		} else
+		{
+            input = Vector2.Lerp(input, Vector2.zero, Time.deltaTime * inputInertiaLerp);
+		}
+
+        camPosition += input;
+        transform.position = Vector3.Lerp(transform.position, camPosition.toV3(35f), Time.deltaTime * cameraTrackingLerp);
+    }
+}
---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utility
+{
+    public static int i;
+    public static float f;
+    public static Vector2 v2;
+    public static Vector3 v3;
+
+    public static Vector2 toV2(this Vector3 v3)
+	{
+        return new Vector2(v3.x, v3.z);
+	}
+
+    public static Vector3 toV3(this Vector2 v2, float y)
+	{
+        return new Vector3(v2.x, y, v2.y);
+	}
+}

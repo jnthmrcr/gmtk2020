980314ee9553cb72bd214d397120f5dda272691b
Jonathan <jonathanmercer2012@yahoo.com>
2020-07-12 11:40:58 -0500

    removed print from PlayerCursor.cs

--- Assets/Scripts/PlayerCursor.cs
+++ Assets/Scripts/PlayerCursor.cs
@@ -15,7 +15,6 @@ public class PlayerCursor : MonoBehaviour
 
 	private void Update()
 	{
-		print(Input.mousePosition);
 		Vector2 mouse = Input.mousePosition;
 		Vector3 worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, Camera.main.transform.position.y));
 		transform.position = new Vector3(Mathf.Round(worldPoint.x), 0, Mathf.Round(worldPoint.z));

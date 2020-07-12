faefc235ef2b6172d918f63edfd2a035ef21a9da
Jonathan <jonathanmercer2012@yahoo.com>
2020-07-12 07:17:59 -0500

    moved playercursor.cs to scripts, changed enemy range indicator behavior

--- Assets/Scripts/PlayerCursor.cs
+++ Assets/Scripts/PlayerCursor.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerCursor : MonoBehaviour
 {
+	[SerializeField] GameObject enemyRangeIndicator;
+	[SerializeField] LayerMask enemyMask;
+
 	private void Start()
 	{
 		Cursor.visible = false;
+		enemyRangeIndicator.transform.parent = null;
 	}
 
 	private void Update()
@@ -15,5 +19,20 @@ public class PlayerCursor : MonoBehaviour
 		Vector2 mouse = Input.mousePosition;
 		Vector3 worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, Camera.main.transform.position.y));
 		transform.position = new Vector3(Mathf.Round(worldPoint.x), 0, Mathf.Round(worldPoint.z));
+
+		if (Physics.CheckSphere(transform.position, 0.1f, enemyMask))
+		{
+			enemyRangeIndicator.SetActive(true);
+			enemyRangeIndicator.transform.position = transform.position;
+		}
+		else
+		{
+			//enemyRangeIndicator.SetActive(false);
+		}
+	}
+
+	private void OnApplicationFocus(bool focus)
+	{
+		Cursor.visible = false;
 	}
 }

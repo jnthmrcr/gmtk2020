519f704883713b2d6b29153c62b454f994cdffb0
Jonathan <jonathanmercer2012@yahoo.com>
2020-07-12 06:43:31 -0500

    game cursor
    new terrain shape (octagon), made enemy a prefab

---
+++ Assets/PlayerCursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCursor : MonoBehaviour
+{
+	private void Start()
+	{
+		Cursor.visible = false;
+	}
+
+	private void Update()
+	{
+		print(Input.mousePosition);
+		Vector2 mouse = Input.mousePosition;
+		Vector3 worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, Camera.main.transform.position.y));
+		transform.position = new Vector3(Mathf.Round(worldPoint.x), 0, Mathf.Round(worldPoint.z));
+	}
+}

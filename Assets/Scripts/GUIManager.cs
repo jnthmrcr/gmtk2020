538bba88ffc7de08fc4212851c5516670092cb13
Jonathan <jonathanmercer2012@yahoo.com>
2020-07-12 07:31:08 -0500

    Canvas prefab/forgotton bits of cursor

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BtnAttack()
+	{
+
+	}
+
+    public void BtnMove()
+	{
+
+	}
+
+    public void BtnForcast()
+	{
+
+	}
+
+    public void BtnMenu()
+	{
+
+	}
+}

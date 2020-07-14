fa1fadb805180316003e2b504f6e39259794034b
Jonathan <jonathanmercer2012@yahoo.com>
2020-07-14 00:14:32 -0500

    files for map node system
    would replace current grid movement node system

---
+++ Assets/Scripts/MapGridSystem/MapNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapNode : MonoBehaviour
+{
+	public bool walkable;
+	public Vector3 worldPosition;
+	public int gridX;
+	public int gridY;
+
+	public int cost = 0;
+	public int windCostX = 0;
+	public int windCostY = 0;
+	public GridNode parent;
+	// ref to either the obstacle or pawn on this node
+
+	public MapNode(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY)
+	{
+		walkable = _walkable;
+		worldPosition = _worldPos;
+		gridX = _gridX;
+		gridY = _gridY;
+	}
+}

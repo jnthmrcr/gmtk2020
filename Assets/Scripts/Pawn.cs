7f7b4286f3ae8ca93a7d9d505f90e2e440cdc929
Jonathan <jonathanmercer2012@yahoo.com>
2020-07-25 23:17:54 -0500

    dont need linq or compiler services...

--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Pawn : MonoBehaviour

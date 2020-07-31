19d3eb135a810d79379accddd9f502d8162588dc
Jonathan <jonathanmercer2012@yahoo.com>
2020-07-30 22:32:51 -0500

    Beginnings of GameManager/game flow systems, cursor gfx

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,58 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public enum gamePhase { environmentTurn, enemyTurn, playerTurn, noTurn }
+	public enum gamePhase { environmentTurn, enemyTurn, playerTurn, noTurn }
+	public gamePhase currentTurnPhase;
 
+	[SerializeField] PlayerController player;
 
+	private void Start()
+	{
+		BeginGame();
+	}
+
+	void BeginGame()
+	{
+		currentTurnPhase = gamePhase.environmentTurn;
+		NextTurn();
+	}
+
+	void NextTurn()
+	{
+		switch (currentTurnPhase)
+		{
+			case gamePhase.environmentTurn:
+				currentTurnPhase = gamePhase.playerTurn;
+				TurnAction(currentTurnPhase);
+				break;
+			case gamePhase.playerTurn:
+				currentTurnPhase = gamePhase.enemyTurn;
+				TurnAction(currentTurnPhase);
+				break;
+			case gamePhase.enemyTurn:
+				currentTurnPhase = gamePhase.playerTurn;
+				TurnAction(currentTurnPhase);
+				break;
+			default:
+				break;
+		}
+	}
+
+	void TurnAction(gamePhase phase)
+	{
+		switch (phase)
+		{
+			case gamePhase.environmentTurn:
+				break;
+			case gamePhase.enemyTurn:
+				break;
+			case gamePhase.playerTurn:
+				player.SetPhaseActive(true);
+				break;
+			case gamePhase.noTurn:
+				break;
+			default:
+				break;
+		}
+	}
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] PlayerCursor myCursor;
+    [SerializeField] List<PlayerMech> myMechs;
+
+    bool phaseActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPhaseActive(bool isActive)
+	{
+        phaseActive = isActive;
+        if (isActive)
+		{
+            myCursor.ShowCursor(true);
+
+			foreach (PlayerMech mech in myMechs)
+			{
+                mech.PhaseInit(0, 0);
+			}
+
+		} else
+		{
+            myCursor.ShowCursor(false);
+        }
+    }
+}

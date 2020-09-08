d4ef8761d50dadc24d2dc06975ca1a8d6a3b5dca
Jonathan <jonathanmercer2012@yahoo.com>
2020-09-07 20:12:15 -0500

    small node positions are set correctly
    .anchored position is a local transform and doesn't need parent's offset

--- Assets/Scripts/UI_WindDisplay.cs
+++ Assets/Scripts/UI_WindDisplay.cs
@@ -17,20 +17,20 @@ public class UI_WindDisplay : MonoBehaviour
 
 	public void SetDisplay(int x, int y)
 	{
-		nodeSmall[0].position = myTrans.position;
+		nodeSmall[0].anchoredPosition = Vector2Int.zero;
 		int index = 1;
 
 		for (int i = 1; i < Mathf.Abs(x) + 1; i++)
 		{
 			nodeSmall[index].gameObject.SetActive(true);
-			nodeSmall[index].anchoredPosition = myTrans.anchoredPosition + new Vector2(Mathf.Sign(x) * i * 20f, 0);
+			nodeSmall[index].anchoredPosition = new Vector2(Mathf.Sign(x) * i * 20f, 0);
 			index++;
 		}
 
 		for (int i = 1; i < Mathf.Abs(y); i++)
 		{
 			nodeSmall[index].gameObject.SetActive(true);
-			nodeSmall[index].anchoredPosition = myTrans.anchoredPosition + new Vector2(x * 20f, Mathf.Sign(y) * i * 20f);
+			nodeSmall[index].anchoredPosition = new Vector2(x * 20f, Mathf.Sign(y) * i * 20f);
 			index++;
 		}
 
@@ -40,10 +40,10 @@ public class UI_WindDisplay : MonoBehaviour
 			index++;
 		}
 
-		nodeLarge.anchoredPosition = myTrans.anchoredPosition + new Vector2(x * 20f, y * 20f);
+		nodeLarge.anchoredPosition = new Vector2(x * 20f, y * 20f);
 
 		connectorLine.transform.localScale = new Vector3(Vector2.Distance(Vector2.zero, new Vector2(x, y)), 1, 1);
-		connectorLine.anchoredPosition = myTrans.anchoredPosition + new Vector2(x * 10f, y * 10f);
+		connectorLine.anchoredPosition = new Vector2(x * 10f, y * 10f);
 		connectorLine.rotation = Quaternion.LookRotation(new Vector3(x, 0, y), Vector3.up) * Quaternion.Euler(90, 90, 0); //Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Acos((float) x / (float) y));
 	}
 }
